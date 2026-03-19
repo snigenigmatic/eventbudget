51383ab85b3d689b44ce491b85b50c70890ec009
snigenigmatic <kaustubhc.1105@gmail.com>
2026-03-19 18:54:33 +0530

    first commit

---
+++ src/main/java/com/eventbudget/EventbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.eventbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventbudget/EventbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
